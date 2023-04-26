4059c2998f0204d94d9aa6af68df2b496ac3108f
James <jtyflol@gmail.com>
2023-04-26 20:58:02 +0800

    Initial Commit

---
+++ BMI Calc/BMI_CalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMI_CalcApp.swift
+//  BMI Calc
+//
+//  Created by James Toh on 26/4/23.
+//
+
+import SwiftUI
+
+@main
+struct BMI_CalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BMI Calc/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  BMI Calc
+//
+//  Created by James Toh on 26/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
